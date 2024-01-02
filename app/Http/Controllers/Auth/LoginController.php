a7754557fcdf645d366705b62816c036ff712f66
waythin <wathinmarma@gmail.com>
2024-01-02 23:05:21 +0600

    project init & Oauth

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Providers\RouteServiceProvider;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+
+    protected function _registerOrLoginUser($data)
+    {
+        $user = User::where('email',$data->email)->first();
+          if(!$user){
+             $user = new User();
+             $user->name = $data->name;
+             $user->email = $data->email;
+             $user->role = 'employee';
+             $user->password = null;
+             $user->save();
+          }
+        Auth::login($user);
+    }
+
+    
+    //Google Login
+    public function redirectToGoogle(){
+        return Socialite::driver('google')->stateless()->redirect();
+        }
+        
+        //Google callback  
+    public function handleGoogleCallback(){
+    
+        $user = Socialite::driver('google')->stateless()->user();
+        
+        $this->_registerorLoginUser($user);
+        return redirect()->route('home');
+    }
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+//google
+Route::get('/login/google', [App\Http\Controllers\Auth\LoginController::class, 'redirectToGoogle'])->name('login.google');
+Route::get('/login/google/callback', [App\Http\Controllers\Auth\LoginController::class, 'handleGoogleCallback']);
